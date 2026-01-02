bc0163ebb6a85cc5c3fcdcf4ede56be6648ff424
tejash098 <jaitej123@gmail.com>
2026-01-02 15:12:23 +0530

    feat: Implement Admin Dashboard with complaint management features and enhance User Dashboard with a complaint detail modal.

--- AdminDashboard/urls.py
+++ AdminDashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.AdminDashboard, name='AdminDashboard'),
+    path('complaint_detail/<int:complaint_id>', views.complaint_detail, name='complaint_detail')
 ]
--- AdminDashboard/views.py
+++ AdminDashboard/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from UserDashboard.models import Complaint 
 from django.contrib.auth.models import User
-# Create your views here.
+from django.shortcuts import redirect
+from django.utils.timezone import now
+from datetime import datetime, timedelta
 
 @login_required(login_url='login')
 def AdminDashboard(request):
@@ -11,8 +13,29 @@ def AdminDashboard(request):
     pendingComplaints = Complaint.objects.filter(status = 'PENDING').count()
     inProgressComplaints = Complaint.objects.filter(status = 'IN_PROGRESS').count()
     resolvedComplaints = Complaint.objects.filter(status = 'RESOLVED').count()
+
+    resolutionRate = (
+        (resolvedComplaints / totalComplaints) * 100
+        if totalComplaints > 0 else 0
+    )
+
+    last_7_days = now() - timedelta(days=7)
+    recentComplaints = complaints.filter(created_at__gte=last_7_days).count()
+
     return render(request, 'AdminDashboard/AdminDashboard.html', {"complaints" : complaints, 
     "totalComplaints" : totalComplaints, 
     "pendingComplaints" : pendingComplaints, 
     "inProgressComplaints" : inProgressComplaints, 
-    "resolvedComplaints" : resolvedComplaints})
+    "resolvedComplaints" : resolvedComplaints,
+    "resolutionRate" : round(resolutionRate,1),
+    "recentComplaints" : recentComplaints})
+
+@login_required(login_url='login')
+def complaint_detail(request, complaint_id):
+    complaint = Complaint.objects.get(id=complaint_id)
+    if request.method == "POST":
+        complaint.status = request.POST.get('status')
+        complaint.admin_remark = request.POST.get('admin_remark')
+        complaint.save()
+        return redirect('AdminDashboard')
+    return render(request, 'AdminDashboard/complaint_detail.html', {'complaint': complaint})
